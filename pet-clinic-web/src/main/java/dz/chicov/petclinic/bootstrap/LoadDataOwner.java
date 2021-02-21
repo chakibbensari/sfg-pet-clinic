012087a43962d7f0cd6ba27dfe8ec413116c92ea
chakib.bensari <chakib_bensari@hotmail.fr>
2021-02-21 18:12:18 +0100

    Exception handlin in CommandeLindeRunner. Closes #23

--- pet-clinic-web/src/main/java/dz/chicov/petclinic/bootstrap/LoadDataOwner.java
+++ pet-clinic-web/src/main/java/dz/chicov/petclinic/bootstrap/LoadDataOwner.java
@@ -5,41 +5,37 @@ import dz.chicov.petclinic.model.Person;
 import dz.chicov.petclinic.model.Vet;
 import dz.chicov.petclinic.services.OwnerService;
 import dz.chicov.petclinic.services.VetService;
-import dz.chicov.petclinic.services.map.OwnerServiceMapImpl;
-import dz.chicov.petclinic.services.map.VetServiceMapImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoadDataOwner implements CommandLineRunner{
 
-    private OwnerService ownerServiceImpl;
-    private VetService vetServiceImpl;
+    private final OwnerService ownerService;
+    private final VetService vetService;
 
-    public LoadDataOwner() {
-        this.ownerServiceImpl = new OwnerServiceMapImpl();
-        this.vetServiceImpl = new VetServiceMapImpl();
+    public LoadDataOwner(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
         Owner owner = createOwner(1L, "Thompson", "John");
-        ownerServiceImpl.save(owner);
-
         Owner owner1 = createOwner(2L, "Shwadrzmûller", "Maximilian");
-        ownerServiceImpl.save(owner1);
-
-        System.out.println("Owners Loaded");
-
         Vet vet = createVet(1L, "Thompson", "John");
-        vetServiceImpl.save(vet);
-
         Vet vet1 = createVet(2L, "Shwadrzmûller", "Maximilian");
-        vetServiceImpl.save(vet1);
-
-        System.out.println("Vets Loaded");
-
+        try {
+            ownerService.save(owner);
+            ownerService.save(owner1);
+            vetService.save(vet1);
+            vetService.save(vet);
+            System.out.println(ownerService.findAll().size() + " Owners Loaded");
+            System.out.println(vetService.findAll().size() + " Vets Loaded");
+        } catch (Exception e){
+            throw new Exception("Error during save", e);
+        }
     }
 
     private Vet createVet(Long id, String firstName, String lastName){
