e4ef22ff5cd990aa0b527aca3e109afdf84d58c5
chakib.bensari <chakib_bensari@hotmail.fr>
2021-02-27 22:38:35 +0100

    Enhance Owner with pets. Closes #37

--- pet-clinic-data/src/main/java/dz/chicov/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/dz/chicov/petclinic/model/Owner.java
@@ -1,10 +1,13 @@
 package dz.chicov.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
 
     private String address;
     private String city;
     private String telephone;
+    private Set<Pet> pets;
 
     public String getAddress() {
         return address;
@@ -28,5 +31,13 @@ public class Owner extends Person{
 
     public void setTelephone(String telephone) {
         this.telephone = telephone;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
