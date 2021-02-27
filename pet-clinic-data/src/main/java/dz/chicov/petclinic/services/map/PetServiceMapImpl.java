82a67b1aaf814892a2f6aea0c2ae1c5798ca1245
chakib.bensari <chakib_bensari@hotmail.fr>
2021-02-27 23:02:51 +0100

    Adding @Service to PetServiceMapImpl. Closes #49

--- pet-clinic-data/src/main/java/dz/chicov/petclinic/services/map/PetServiceMapImpl.java
+++ pet-clinic-data/src/main/java/dz/chicov/petclinic/services/map/PetServiceMapImpl.java
@@ -1,9 +1,22 @@
 package dz.chicov.petclinic.services.map;
 
+import dz.chicov.petclinic.model.Owner;
 import dz.chicov.petclinic.model.Pet;
+import dz.chicov.petclinic.model.PetType;
 import dz.chicov.petclinic.services.CRUDService;
+import dz.chicov.petclinic.services.OwnerService;
+import dz.chicov.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PetServiceMapImpl extends AbstractServiceMap<Pet, Long> implements CRUDService<Pet, Long> {
+
+    private PetTypeService petTypeService;
+
+    public PetServiceMapImpl(PetTypeService petTypeService) {
+        this.petTypeService = petTypeService;
+    }
+
     @Override
     public Pet findById(Long id) {
         return super.findById(id);
@@ -11,6 +24,16 @@ public class PetServiceMapImpl extends AbstractServiceMap<Pet, Long> implements 
 
     @Override
     public Pet save(Pet pet) {
+        if(pet != null){
+            if(pet.getPetType() != null){
+                if(pet.getPetType().getId() == null){
+                    PetType petType = petTypeService.save(pet.getPetType());
+                    pet.getPetType().setId(petType.getId());
+                }
+            }
+        } else {
+            throw new RuntimeException("Cannot save a null Pet");
+        }
         return super.save(pet);
     }
 
