d25e339e6d93f2e6db1acf6047cee11621e63c9b
chakib.bensari <chakib_bensari@hotmail.fr>
2021-02-27 22:32:27 +0100

    Create Specialty entity and add it to Vet Entity. Closes #31

---
+++ pet-clinic-data/src/main/java/dz/chicov/petclinic/model/Specialty.java
@@ -0,0 +1,4 @@
+package dz.chicov.petclinic.model;
+
+public class Specialty extends NamedEntity{
+}
--- pet-clinic-data/src/main/java/dz/chicov/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/dz/chicov/petclinic/model/Vet.java
@@ -1,4 +1,27 @@
 package dz.chicov.petclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Vet extends Person{
+
+    private Set<Specialty> specialties = new HashSet<>();
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public Set<Specialty> addSpecialty(Specialty specialty){
+        this.specialties.add(specialty);
+        return this.specialties;
+    }
+
+    public Set<Specialty> removeSpecialty(Specialty specialty){
+        this.specialties.remove(specialty);
+        return this.specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
