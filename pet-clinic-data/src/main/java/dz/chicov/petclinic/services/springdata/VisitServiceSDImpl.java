76533ad4eba380f0b5daaaf854ef67593ef16485
chakib.bensari <chakib_bensari@hotmail.fr>
2021-03-02 13:51:57 +0100

    Spring Data JPA implementation for VisitService. Closes #53

---
+++ pet-clinic-data/src/main/java/dz/chicov/petclinic/services/springdata/VisitServiceSDImpl.java
@@ -0,0 +1,48 @@
+package dz.chicov.petclinic.services.springdata;
+
+import dz.chicov.petclinic.model.Visit;
+import dz.chicov.petclinic.repositories.VisitRepository;
+import dz.chicov.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdata")
+public class VisitServiceSDImpl implements VisitService {
+
+    private VisitRepository visitRepository;
+
+    public VisitServiceSDImpl(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return visitRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return visitRepository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        visitRepository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        visitRepository.deleteById(id);
+    }
+}
