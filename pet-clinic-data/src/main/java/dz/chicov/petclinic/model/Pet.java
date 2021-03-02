328f0b22f31fc00c1bc230ae1fa4d99438a53933
chakib.bensari <chakib_bensari@hotmail.fr>
2021-03-02 10:02:13 +0100

    Implementing Pet, PetType Entities with JPA. Closes #56

--- pet-clinic-data/src/main/java/dz/chicov/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/dz/chicov/petclinic/model/Pet.java
@@ -1,11 +1,21 @@
 package dz.chicov.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends NamedEntity{
 
+    @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     public LocalDate getBirthDate() {
