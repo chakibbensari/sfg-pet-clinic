4ebb9ad8584b9b61a53599c422d1f012c4fd63e9
chakib.bensari <chakib_bensari@hotmail.fr>
2021-03-02 09:53:30 +0100

    Implementing Owner Entity with JPA. Closes #40

--- pet-clinic-data/src/main/java/dz/chicov/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/dz/chicov/petclinic/model/Owner.java
@@ -1,13 +1,23 @@
 package dz.chicov.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person{
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
@@ -36,6 +46,16 @@ public class Owner extends Person{
 
     public Set<Pet> getPets() {
         return pets;
+    }
+
+    public Owner addPet(Pet pet){
+        getPets().add(pet);
+        return this;
+    }
+
+    public Owner removePet(Pet pet){
+        getPets().remove(pet);
+        return this;
     }
 
     public void setPets(Set<Pet> pets) {
